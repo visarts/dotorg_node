3b875579e04840f549d5d97c81521757c994930c
brendanBest <brendan.best@target.com>
2018-08-07 08:39:47 -0500

    add dependencies and create structure for routes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = 8000
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
