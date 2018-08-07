448e5fe902fc41a3290d8df2ce10e584906279d7
visarts <visarts@gmail.com>
2018-08-07 11:00:29 -0500

    complete setup of routes and api

--- app/routes/main_routes.js
+++ app/routes/main_routes.js
@@ -1,5 +1,57 @@
+const ObjectID = require('mongodb').ObjectID
+
+const collectionName = 'artists'
+
 module.exports = function (app, db) {
+  app.get('/', (req, res) => {
+    res.send('<h1 style="color: #56a; font: bold 2em sans-serif;">its a GET call</h1>')
+  })
+
   app.post('/post', (req, res) => {
-    res.send('post call successful')
+    const artist = { name: req.body.name, longName: req.body.longName }
+    db.collection(collectionName).insert(artist, (err, result) => {
+      if (err) {
+        res.send({ error: 'oops there\'s an error' })
+      } else {
+        res.send(result.ops[0])
+      }
+    })
+  })
+
+  app.get('/artists/:id', (req, res) => {
+    const id = req.params.id
+    const details = { '_id': new ObjectID(id) }
+    db.collection(collectionName).findOne(details, (err, item) => {
+      if (err) {
+        res.send({ error: 'oops there\'s an error' })
+      } else {
+        res.send(item)
+      }
+    })
+  })
+
+  app.delete('/artists/:id', (req, res) => {
+    const id = req.params.id
+    const details = { '_id': new ObjectID(id) }
+    db.collection(collectionName).remove(details, (err, item) => {
+      if (err) {
+        res.send({ error: 'oops there\'s an error' })
+      } else {
+        res.send(`Artist ${id} deleted`)
+      }
+    })
+  })
+
+  app.put('/artists/:id', (req, res) => {
+    const id = req.params.id
+    const details = { '_id': new ObjectID(id) }
+    const artist = { name: req.body.name, longName: req.body.longName }
+    db.collection(collectionName).update(details, artist, (err, result) => {
+      if (err) {
+        res.send({ error: 'oops there\'s an error' })
+      } else {
+        res.send(artist)
+      }
+    })
   })
 }
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
+const db = require('./config/db')
 
 const app = express()
 
 const port = 8000
 
-require('./app/routes')(app, {})
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+const dbName = 'portitude'
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database.db(dbName))
+
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+  })
 })
